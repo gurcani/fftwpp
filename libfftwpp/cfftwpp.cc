574fc530d0edf5b3fc40eefa325db707fc1e562f
gurcani <ozgur.gurcan@lpp.polytechnique.fr>
2020-12-02 19:39:24 +0100

    adding new multiplier functions

--- libfftwpp/cfftwpp.cc
+++ libfftwpp/cfftwpp.cc
@@ -11,7 +11,8 @@ extern "C" {
   // This 2D version of the scheme of Basdevant, J. Comp. Phys, 50, 1983
   // applied to passive scalar problem. Requires 7 FFTs per stage, with 3 inputs and 4 outputs.
   // one could also do 4 inputs and 3 outputs.
-  void multadvection4(double **F, unsigned int m,
+  
+  void multadvection34(double **F, unsigned int m,
 		      const unsigned int indexsize,
 		      const unsigned int *index,
 		      unsigned int r, unsigned int threads)
@@ -50,7 +51,7 @@ extern "C" {
     for(unsigned int j=0; j < m; ++j) {
       double u=F0[j];
       double v=F1[j];
-      double n=F2[m1];
+      double n=F2[j];
       F0[j]=v*v-u*u;
       F1[j]=u*v;
       F2[j]=n*u;
@@ -58,4 +59,60 @@ extern "C" {
     }
 #endif
   }
+
+  void multadvection62(double **F, unsigned int m,
+		      const unsigned int indexsize,
+		      const unsigned int *index,
+		      unsigned int r, unsigned int threads)
+  {
+    double* F0=F[0];
+    double* F1=F[1];
+    double* F2=F[2];
+    double* F3=F[3];
+    double* F4=F[4];
+    double* F5=F[5];
+#ifdef __SSE2__
+    unsigned int m1=m-1;
+    PARALLEL(
+	     for(unsigned int j=0; j < m1; j += 2) {
+	       double *F0j=F0+j;
+	       double *F1j=F1+j;
+	       double *F2j=F2+j;
+	       double *F3j=F3+j;
+	       double *F4j=F4+j;
+	       double *F5j=F5+j;
+	       Vec dxphi=LOAD(F0j);
+	       Vec dyphi=LOAD(F1j);
+	       Vec dxom=LOAD(F2j);
+	       Vec dyom=LOAD(F3j);
+	       Vec dxn=LOAD(F4j);
+	       Vec dyn=LOAD(F5j);
+	       STORE(F0j,dxphi*dyom-dyphi*dxom);
+	       STORE(F1j,dxphi*dyn-dyphi*dxn);
+	     }
+	     );
+    if(m % 2) {
+      double dxphi=F0[m1];
+      double dyphi=F1[m1];
+      double dxom=F2[m1];
+      double dyom=F3[m1];
+      double dxn=F4[m1];
+      double dyn=F5[m1];
+      F0[m1]=dxphi*dyom-dyphi*dxom;
+      F1[m1]=dxphi*dyn-dyphi*dxn;
+    }
+#else
+    for(unsigned int j=0; j < m; ++j) {
+      double dxphi=F0[j];
+      double dyphi=F1[j];
+      double dxom=F2[j];
+      double dyom=F3[j];
+      double dxn=F4[j];
+      double dyn=F5[j];
+      F0[j]=dxphi*dyom-dyphi*dxom;
+      F1[j]=dxphi*dyn-dyphi*dxn;
+    }
+#endif
+  }
+  
 }
--- libfftwpp/cfftwpp.h
+++ libfftwpp/cfftwpp.h
@@ -13,6 +13,8 @@ extern "C" {
   
   mpiOptions dfoptions;
 
+  realmultiplier multadvection34;
+  
   double __complex__ *fftwpp_complexalign(unsigned int n){
     return (double __complex__ *) ComplexAlign(n);
   }
@@ -61,15 +63,42 @@ extern "C" {
     unsigned int my=(ny+1)/2;
     return new ImplicitHConvolution2MPI(mx,my,xcomp,ycomp,dg,du,(Complex *)g,dfoptions,2,2);
   }
-
   
   void fftwpp_hconv2d_mpi_convolve_adv2(ImplicitHConvolution2MPI* hconv, double __complex__ *f, double __complex__ *g) {
     Complex *G[]={(Complex *)f,(Complex *)g};
     hconv->convolve(G,multadvection2);
   }
   
-#ifdef  __cplusplus
+  ImplicitHConvolution2MPI* fftwpp_create_hconv2d_mpi_adv34(split &dg,split &du,
+							   bool xcomp, bool ycomp, double __complex__ *g){
+    unsigned int nx=dg.X,ny=(dg.Y-1)*2+xcomp;
+    unsigned int nyp=ny/2+1;
+    unsigned int mx=(nx+1)/2;
+    unsigned int my=(ny+1)/2;
+    return new ImplicitHConvolution2MPI(mx,my,xcomp,ycomp,dg,du,(Complex *)g,dfoptions,3,4);
   }
+  
+  void fftwpp_hconv2d_mpi_convolve_adv34(ImplicitHConvolution2MPI* hconv, double __complex__ *f, double __complex__ *g, double __complex__ *h, double __complex__ *r) {
+    Complex *G[]={(Complex *)f,(Complex *)g, (Complex *)h, (Complex *)r};
+    hconv->convolve(G,multadvection34);
+  }
+
+  ImplicitHConvolution2MPI* fftwpp_create_hconv2d_mpiAB(split &dg,split &du,
+							bool xcomp, bool ycomp, double __complex__ *g, unsigned int A, unsigned int B){
+    unsigned int nx=dg.X,ny=(dg.Y-1)*2+xcomp;
+    unsigned int nyp=ny/2+1;
+    unsigned int mx=(nx+1)/2;
+    unsigned int my=(ny+1)/2;
+    return new ImplicitHConvolution2MPI(mx,my,xcomp,ycomp,dg,du,(Complex *)g,dfoptions,A,B);
+  }
+  
+  void fftwpp_hconv2d_mpi_convolve_adv(ImplicitHConvolution2MPI* hconv, double __complex__ **Gp, realmultiplier mul) {
+    Complex **G = (Complex **) Gp;
+    hconv->convolve((Complex**)G,mul);
+  }
+  
+#ifdef  __cplusplus
+}
 #endif
 
 #endif
